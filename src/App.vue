72118dd80768b419d936c35550c6ebe152593452
Yannan23 <yannansun0723@gmail.com>
2025-01-19 18:12:10 +1100

    app initialization

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1>Todo App</h1>
+  <form id="form">
+    <input type="text" placeholder="Enter your todo" id="input" autocomplete="off" class="input">
+    <div class="todo-button">Add Todo</div>
+    <!-- <ul id="todos" class="todos">
+    </ul> -->
+  </form>
+</template>
